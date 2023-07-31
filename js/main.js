14cd5e8ef6afe4c6c2359ac38aa5d17117734c83
Karen Penedo <penedok12@gmail.com>
2023-07-30 21:43:57 -0400

    validando cpf

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 //validacao de CEP
 async function buscandoCep(ceps){
    try{
@@ -27,6 +28,78 @@ async function buscandoCep(ceps){
 var inputCEP = document.querySelector("#cep");
 
 inputCEP.addEventListener("focusout", ()=> buscandoCep (inputCEP.value))
+
+
+//
+var cpf = document.querySelector('#cpf');
+
+function isCPF(cpf =0){
+   cpf = cpf.replace(/\.|-/g,"");
+   let soma= 0; 
+   soma +=  cpf[0] *10;                      //vai implementar 
+   soma +=  cpf[1] *9; 
+   soma +=  cpf[2]  *8
+   soma +=  cpf[3] *7; 
+   soma +=  cpf[4] *6; 
+   soma +=  cpf[5] *5; 
+   soma +=  cpf[6] *4; 
+   soma +=  cpf[7] *3; 
+   soma +=  cpf[8] *2; 
+   soma = (soma*10) %11;
+
+
+
+   if(soma ==10 || soma ==11 )
+    soma=0;
+
+  
+
+   soma +=  cpf[0] *11;                      //vai implementar 
+   soma +=  cpf[1] *10; 
+   soma +=  cpf[2]  *9
+   soma +=  cpf[3] *8; 
+   soma +=  cpf[4] *7; 
+   soma +=  cpf[5] *6; 
+   soma +=  cpf[6] *5; 
+   soma +=  cpf[7] *4; 
+   soma +=  cpf[8] *3; 
+   soma +=  cpf[9] *2;
+   soma = (soma*10) %11; 
+   if(soma ==10 || soma ==11 )
+   soma=0;
+
+
+   if(soma != cpf[10] && cpf[9]) {
+      false
+      alert('CPF invalido')
+      return
+      
+   }
+
+  
+
+   console.log('primeiro dg ' + soma)
+   return true;
+
+}
+cpf.addEventListener('focusout',()=> isCPF(cpf.value))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
